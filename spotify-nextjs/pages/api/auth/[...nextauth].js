2294121a6107954f11d98383a8ccaf0c15b81c64
Viraj Jadhav <viraj28j@gmail.com>
2021-12-11 21:02:19 +0530

    feat expire token

--- spotify-nextjs/pages/api/auth/[...nextauth].js
+++ spotify-nextjs/pages/api/auth/[...nextauth].js
@@ -1,5 +1,30 @@
 import NextAuth from "next-auth"
-import SpotifyProvider from "next-auth/providers/spotify"
+import SpotifyProvider from "next-auth/providers/spotify";
+import spotifyApi, { LOGIN_URL } from "../../../lib/spotify";
+
+async function refreshAccessToken(token) {
+    try {
+        spotifyApi.setAccessToken(token.accessToken);
+        spotifyApi.setRefreshToken(token.refreshToken);
+
+        const { body:refreshedToken } = await spotifyApi.refreshAccessToken();
+        console.log('refreshedToken', refreshedToken);
+
+        return {
+          ...token,
+          accessToken: refreshedToken.access_token,
+          accessTokenExpire
+        }
+    }
+    catch (err) {
+      console.error(err);
+
+      return {
+        ...token,
+        error: "RefreshAccessTokenError",
+      };
+    }
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -11,4 +36,33 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
-})
+  secret: process.env.JWT_SECRET,
+  pages: {
+    signIn: '/login'
+  },
+  callbacks: {
+    async jwt ({ token, user, account }){
+      // initial sign in
+      if (account && user ) {
+        return {
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000,
+          // we are handling expiry times in milliseconds hence * 1000
+        }
+      }
+
+      // Return previous token if the access token has not expired yet
+      if (Date.now() < token.accessTokenExpires) {
+        console.log('token not expired');
+        return token;
+      }
+
+      // Access token has expired, so we need to refresh it...
+      console.log('token expired');
+      return await this.refreshAccessToken(token);
+    },
+    },
+});
